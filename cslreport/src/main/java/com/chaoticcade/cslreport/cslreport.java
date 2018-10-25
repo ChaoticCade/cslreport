384266e2c397c07a99d085f5b2913d83596c8830
ChaoticCade <jay1390@gmail.com>
2018-10-25 13:37:40 -0400

    Runs from memory and cleaned up the log entry builder.

--- cslreport/src/main/java/com/chaoticcade/cslreport/cslreport.java
+++ cslreport/src/main/java/com/chaoticcade/cslreport/cslreport.java
@@ -3,12 +3,10 @@ package com.chaoticcade.cslreport;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
@@ -17,12 +15,12 @@ import java.util.Map;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
 public class cslreport extends JavaPlugin implements Listener {
+	Map<Integer, String> cachedData = new HashMap<Integer, String>();
 
 	@Override
 	public void onEnable() {
@@ -43,7 +41,6 @@ public class cslreport extends JavaPlugin implements Listener {
 			return false;
 		}
 		if (cmd.getName().equalsIgnoreCase("csrep") && (args[0]).equalsIgnoreCase("search") && isStringInt(args[1]) == true){
-
 			//search command
 			if (Integer.parseInt(args[1]) > 7){
 				sender.sendMessage("Can not search more than 7 days back!");
@@ -57,19 +54,16 @@ public class cslreport extends JavaPlugin implements Listener {
 
 				try {
 					readLog(sender, Integer.parseInt(args[1]));
-
 				}
 				catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-
 			}
 			return true;
 		}
 
 		if (cmd.getName().equalsIgnoreCase("csrep") && (args[0]).equalsIgnoreCase("page") && isStringInt(args[1]) == true){        
-
 			//scroll through data command
 			try {
 				browseLog(sender, Integer.parseInt(args[1]));
@@ -88,12 +82,8 @@ public class cslreport extends JavaPlugin implements Listener {
 		return false;
 	}
 
-
 	public void readLog(CommandSender sender, Integer days) throws Exception {
 		sender.sendMessage("Searching...");
-		String workingFile = (getDataFolder() + "\\" + (((Player) sender).getUniqueId()).toString() + ".log");
-		Files.deleteIfExists(Paths.get(workingFile));
-		FileWriter fr = new FileWriter(workingFile, true);
 		String reader = sender.getName().toString();
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 		List<String> dateList = new ArrayList<String>();
@@ -110,8 +100,8 @@ public class cslreport extends JavaPlugin implements Listener {
 				d++;
 			}
 		}
-
 		int w = 0;
+		int k = 0;
 		while (w < dateList.size()) {
 			File log = new File(dateList.get(w));
 			if (Files.exists(Paths.get(dateList.get(w))) == true) {
@@ -120,59 +110,27 @@ public class cslreport extends JavaPlugin implements Listener {
 				@SuppressWarnings("unused")
 				int i = 0;
 				while ((line = br.readLine()) != null) {
-					List<String> myList = new ArrayList<String>(Arrays.asList(line.split(" ")));
-					if (myList.lastIndexOf(reader) == 9) {
-						String entry = myList.get(2) + " " + myList.get(3) + " " + myList.get(4) + " " + myList.get(5) + " "
-								+ myList.get(6) + " " + myList.get(7) + " " + myList.get(8) + " you " + myList.get(10) + " "
-								+ myList.get(11) + " " + myList.get(12) + " " + myList.get(13) + " " + myList.get(14)
-								+ '\n';
-						fr.write(entry);
+					if (line.contains(reader) == true) {
+						String entry = line.replace(reader, "you").substring(19);
+						cachedData.put(k, entry);
+						k++;
 					}
-					if (myList.lastIndexOf(reader) == 10) {
-						String entry = myList.get(2) + " " + myList.get(3) + " " + myList.get(4) + " " + myList.get(5) + " "
-								+ myList.get(6) + " " + myList.get(7) + " " + myList.get(8) + " " + myList.get(9) + " you "
-								+ myList.get(11) + " " + myList.get(12) + " " + myList.get(13) + " " + myList.get(14) + " "
-								+ myList.get(15) + '\n';
-						fr.write(entry);
-					}
-
 					i++;
 				}
 				br.close();
 			}
 			w++;
 		}
-		fr.close();
-		BufferedReader lineCounter = new BufferedReader(new FileReader(workingFile));
-		int lines = 0;
-		while (lineCounter.readLine() != null) lines++;
-		lineCounter.close();
-		sender.sendMessage("Search complete! You have " + (int) Math.ceil((lines / 5.0))+ " pages of data. Use /csrep page # to browse data.");
+		sender.sendMessage("Search complete! You have " + (int)Math.ceil(cachedData.size() / 5.0)+ " pages of data. Use /csrep page # to browse data.");
 
 	}
 
 	public void browseLog(CommandSender sender, Integer pageNumber) throws Exception {
-		Map<String, String> transactionData = new HashMap<String, String>();
-		String file = (getDataFolder() + "\\" + (((Player) sender).getUniqueId()).toString() + ".log");
-		BufferedReader br = new BufferedReader(new FileReader(file));  
-		String line = null;
-		int key = 0;
-		while ((line = br.readLine()) != null) {
-			String keyString = Integer.toString(key);
-			transactionData.put(keyString, line);
-			key++;
-		} 
-		br.close();
-
-		//Iterate from startLine to endLine
 		int startLine = ((pageNumber - 1) * 5);
 		int endLine = (pageNumber * 5);
-
-
-
 		while (startLine < endLine) {
-			if (transactionData.get(Integer.toString(startLine)) != null) {
-				sender.sendMessage((startLine + 1) + ". " + transactionData.get(Integer.toString(startLine)));
+			if (cachedData.get(startLine) != null) {
+				sender.sendMessage((startLine + 1) + ". " + cachedData.get(startLine));
 				startLine++;
 			}
 			else {
@@ -180,19 +138,15 @@ public class cslreport extends JavaPlugin implements Listener {
 				break;
 			}
 		}
-
-
 	}
-
 
 	public boolean isStringInt(String s)
 	{
-		try
-		{
+		try {
 			Integer.parseInt(s);
 			return true;
-		} catch (NumberFormatException ex)
-		{
+		} 
+		catch (NumberFormatException ex) {
 			return false;
 		}
 	}
